42074d5c632658b956aa425dafda0adb7d5a2684
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-23 13:51:11 -0300

    Atualizando landing page

--- landing-page/v2.0/src/script.js
+++ landing-page/v2.0/src/script.js
@@ -1,8 +1,8 @@
 var nav = document.querySelector('nav');
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 100) {
-    nav.classList.add('bg-dark', 'shadow');
+    nav.classList.add('bg-black', 'shadow');
   } else {
-    nav.classList.remove('bg-dark', 'shadow');
+    nav.classList.remove('bg-black', 'shadow');
   }
 })
