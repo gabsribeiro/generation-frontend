18e46604efdffbd862da5b2f396d091cf9db0e87
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-24 16:53:19 -0300

    Add favicon

--- landing-page/v2.0/src/script.js
+++ landing-page/v2.0/src/script.js
@@ -6,3 +6,54 @@ window.addEventListener('scroll', function () {
     nav.classList.remove('bg-black', 'shadow');
   }
 })
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const submitInput = document.querySelector('#submitbtn');
+const errorMessage = document.querySelector('#txtname')
+let nameOk = false;
+let emailOk = false;
+
+submitInput.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const nameValue = nameInput.value;
+  const emailValue = emailInput.value;
+
+  if (nameValue === '' || emailValue === '')
+    errorMessage.textContent = 'preencha todos os campos';
+    errorMessage.style.color = 'red';
+  setTimeout(() => {
+    errorMessage.textContent = '';
+  }, 2000);
+  return;
+});
+
+function ValidationName() {
+  let txtName = document.querySelector('#txtname');
+  if (nameInput.value.length < 3) {
+    txtName.innerHTML = 'nome inválido';
+    txtName.style.color = 'red';
+  } else {
+    txtName.style.display = 'none';
+    nameOk = true;
+  }
+}
+
+function ValidationEmail() {
+  let txtEmail = document.querySelector('#txtemail');
+  if (emailInput.value.indexOf('@') == -1 || emailInput.value.indexOf('.') == -1) {
+    txtEmail.innerHTML = 'e-mail inválido';
+    txtEmail.style.color = 'red';
+  } else {
+    txtEmail.style.display = 'none';
+    emailOk = true;
+  }
+}
+
+function SendValidation() {
+  if (nameOk == true && emailOk == true) {
+    alert('Cadastro realizado com sucesso');
+  }
+}
+
